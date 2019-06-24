6588298dc9bdd031a1f7b8a648bb1f90732dbe40
Ninad Sumant <44379598+ninadsumant@users.noreply.github.com>
2019-06-24 11:45:01 +0530

    Delete brain.r

--- brain.r
+++
@@ -1,9 +0,0 @@
-library(oro.nifti)
-library(papayar)
-library(neurobase)
-img = readNIfTI("Template-T1-U8-RALPFH-BR.nii", reorient = TRUE)
-hist(c(img))
-ortho2(img, img>1e2)
-
-mask = img>1e3
-papaya(list(img,mask))
